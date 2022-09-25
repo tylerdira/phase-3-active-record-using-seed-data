37d099e5320751ebfd855405f62c2c405932d60d
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-09-25 16:14:49 -0700

    commit tree 7b03c57452a46e11cbc6aaded2494bb214ddb32d

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,13 @@
 # Add a console message so we can see output when the seed file runs
+puts "seeding games..."
+
+50.times do
+    Game.create(
+        title: Faker::Game.title,
+        genre: Faker::Game.genre,
+        platform: Faker::Game.platform,
+        price: rand(0..60)
+    )
+end
+
+puts "Done!"
